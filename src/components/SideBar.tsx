929facf9bc7974f68cb0b23c79f25b5db1b3f1ce
Diego França <diego.franca92@gmail.com>
2023-08-07 20:39:34 -0300

    feat: close drawer action

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -17,8 +17,11 @@ import { Link } from 'react-router-dom'
 import React from 'react'
 import useAuth from '../hooks/useAuth'
 
-export function SideBar() {
+type NavProps = {
+  setClose?(data: boolean): void
+}
 
+export function SideBar() {
   return (
     <Card className='min-h-full h-screen w-full max-w-[18rem] p-4 shadow-xl'>
       <div className='mb-2 flex items-center justify-between p-4'>
@@ -30,22 +33,29 @@ export function SideBar() {
   )
 }
 
-function NavList() {
+function NavList({ setClose }: NavProps) {
   const { signOut } = useAuth()
 
   async function handleLogout() {
+    closeMenu()
     signOut()
+  }
+
+  function closeMenu() {
+    if (setClose) {
+      setClose(false)
+    }
   }
 
   return (
     <List>
-      <Link to='/home'>
+      <Link to='/home' onClick={closeMenu}>
         <ListItem>
           <HomeIcon className='h-6 w-6 mr-4' />
           Home
         </ListItem>
       </Link>
-      <Link to='/profile' className='pointer-events-none'>
+      <Link to='/profile' className='pointer-events-none' onClick={closeMenu}>
         <ListItem disabled>
           <UserCircleIcon className='h-6 w-6 mr-4' />
           Perfil
@@ -53,13 +63,12 @@ function NavList() {
       </Link>
       <hr className='my-2 border-blue-gray-50' />
       <ListItem onClick={handleLogout}>
-          <ArrowLeftOnRectangleIcon className='h-6 w-6 mr-4' />
-          Sair
-        </ListItem>
+        <ArrowLeftOnRectangleIcon className='h-6 w-6 mr-4' />
+        Sair
+      </ListItem>
     </List>
   )
 }
-
 
 export function MenuDrawer() {
   const [open, setOpen] = React.useState(false)
@@ -68,22 +77,22 @@ export function MenuDrawer() {
 
   return (
     <React.Fragment>
-      <div className="mb-2 flex items-center justify-between fixed top-0 shadow-sm h-[60px] bg-white z-50 w-screen p-4">
-        <img src={logoPontua} className="w-3/12" alt="Logomarca Pontua" />
+      <div className='mb-2 flex items-center justify-between fixed top-0 shadow-sm h-[60px] bg-white z-50 w-screen p-4'>
+        <img src={logoPontua} className='w-3/12' alt='Logomarca Pontua' />
         <IconButton onClick={openDrawer} className='bg-pontua-primary'>
           <Bars3Icon className='h-8 w-8 text-white' />
         </IconButton>
       </div>
 
       <Drawer open={open} onClose={closeDrawer}>
-        <div className="mb-2 flex items-center justify-between p-4">
-          <img src={logoPontua} className="w-3/5" alt="Logomarca Pontua" />
-          <IconButton variant="text" color="blue-gray" onClick={closeDrawer}>
+        <div className='mb-2 flex items-center justify-between p-4'>
+          <img src={logoPontua} className='w-3/5' alt='Logomarca Pontua' />
+          <IconButton variant='text' color='blue-gray' onClick={closeDrawer}>
             <XMarkIcon className='h-6 w-6' />
           </IconButton>
         </div>
-        <hr className="my-2 border-blue-gray-50" />
-        <NavList />
+        <hr className='my-2 border-blue-gray-50' />
+        <NavList setClose={data => setOpen(data)} />
       </Drawer>
     </React.Fragment>
   )
